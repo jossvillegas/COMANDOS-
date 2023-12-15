41d7a609ca04fd9a8b4a7f9e291c1f865f611f9a
jossvillegas <150206999+jossvillegas@users.noreply.github.com>
2023-12-15 08:49:43 -0500

    Add files via upload

---
+++ Ciclo2.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ciclo2;
+
+/**
+ *
+ * @author pc
+ */
+public class Ciclo2 {
+
+    public static void main(String[] args) {
+        String [] nombres ={"Paula","Alex","Andrea","Byron","Heidy"
+    };
+        for (String i:nombres){
+            System.out.println("Nombre: "+i);
+        }
+    
+    }
+    
+      
+}
