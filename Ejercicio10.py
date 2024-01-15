a4733e3f8d9fb6898a5c532301c10c3a525fbe46
jossvillegas <150206999+jossvillegas@users.noreply.github.com>
2024-01-15 11:09:18 -0500

    Add files via upload

---
+++ Ejercicio10.py
@@ -0,0 +1,12 @@
+class MiClase:
+    def __init__(self) :
+        self._atributo_privado = "Valor"
+     
+    def _hablar(self):
+        print("Hola,como estas")
+
+
+objecto = MiClase()
+print(objecto._hablar())
+
+        
