32c6b8441c30d33d99762faac86bfd1443e46ac5
jossvillegas <150206999+jossvillegas@users.noreply.github.com>
2024-01-15 11:09:04 -0500

    Add files via upload

---
+++ Ejercicio9.py
@@ -0,0 +1,8 @@
+def recorrer(elemento):
+    for item in elemento:
+        print(f"El elemento actual es :{item}")
+
+lista =[1,2,3,4,5,6,7,8,9]
+lista2 =["maquina","como","andas"]       
+
+recorrer(lista)
